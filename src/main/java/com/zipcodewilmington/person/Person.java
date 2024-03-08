d1da16131527daedcc749a24c9fd212fda983b32
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-03-07 21:23:04 -0500

    committed

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,55 @@ package com.zipcodewilmington.person;
  * If you did ObjectsOne lab, you might recognize this code?
  */
 public class Person {
-    private String name;
-    private int age;
-
-    public Person() {
+    private String name ="";
+    private int age=Integer.MAX_VALUE;
+    private String gender ="";
+    private int weight=Integer.MAX_VALUE;
+    public Person(String name, int age, String gender, int weight) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.weight = weight;
     }
+    public Person(){
 
+    }
     public Person(int age) {
+        this.age = age;
     }
-
     public Person(String name) {
+         this.name = name;
     }
 
-    public Person(String name, int age) {
+    public Person(int age, String name) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
-
     public void setAge(int age) {
+        this.age = age;
     }
-
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
     public String getName() {
-        return null;
+        return this.name;
     }
-
     public Integer getAge() {
-        return null;
+        return this.age;
+    }
+    public String getGender() {
+        return this.gender;
+    }
+    public Integer getWeight() {
+        return this.weight;
     }
 }
+
+
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -12,58 +12,70 @@ public class TestPerson {
         // Given
         String expectedName = "";
         Integer expectedAge = Integer.MAX_VALUE;
-
+        String expectedGender = "";
+        Integer expectedWeight = Integer.MAX_VALUE;
         // When
         Person person = new Person();
-
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
-
+        String actualGender = person.getGender();
+        Integer actualWeight = person.getWeight();
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedWeight, actualWeight);
     }
-
     @Test
     public void testConstructorWithName() {
         // Given
         String expected = "Leon";
-
         // When
         Person person = new Person(expected);
-
         // Then
         String actual = person.getName();
         Assert.assertEquals(expected, actual);
     }
-
     @Test
     public void testConstructorWithAge() {
         // Given
         Integer expected = 5;
-
         // When
         Person person = new Person(expected);
-
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
-
-
+    @Test
+    public void testConstructorWithGender() {
+        // Given
+        String expected = "Female";
+        // When
+        Person person = new Person(expected);
+        // Then
+        String actual = person.getGender();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testConstructorWithWeight() {
+        // Given
+        Integer expected = 100;
+        // When
+        Person person = new Person(expected);
+        // Then
+        Integer actual = person.getWeight();
+        Assert.assertEquals(expected, actual);
+    }
     @Test
     public void testConstructorWithNameAndAge() {
         // Given
         Integer expectedAge = 5;
         String expectedName = "Leon";
-
         // When
-        Person person = new Person(expectedName, expectedAge);
-
-        // Then
+        Person person = new Person(expectedAge, expectedName);
+        /* Then */
         Integer actualAge = person.getAge();
         String actualName = person.getName();
-
         Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
     }
@@ -73,26 +85,43 @@ public class TestPerson {
         // Given
         Person person = new Person();
         String expected = "Leon";
-
         // When
         person.setName(expected);
         String actual = person.getName();
-
         // Then
         Assert.assertEquals(expected, actual);
     }
-
     @Test
     public void testSetAge() {
         // Given
         Person person = new Person();
         Integer expected = 5;
-
         // When
         person.setAge(expected);
-
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetGender() {
+        // Given
+        Person person = new Person();
+        String expected = "Female";
+        // When
+        person.setGender(expected);
+        // Then
+        String actual = person.getGender();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetWeight() {
+        // Given
+        Person person = new Person();
+        Integer expected = 160;
+        // When
+        person.setWeight(expected);
+        // Then
+        Integer actual = person.getWeight();
         Assert.assertEquals(expected, actual);
     }
 }
